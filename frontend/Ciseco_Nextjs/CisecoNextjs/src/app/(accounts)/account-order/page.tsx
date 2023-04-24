770acc7cf6a3b88e88f43cf27454de559dab6e52
Shashwat Gokhe <Shashwatgokhe2@gmail.com>
2023-04-25 02:42:21 +0530

    3rd push
    neend aa rahi hai bhai

--- frontend/Ciseco_Nextjs/CisecoNextjs/src/app/(accounts)/account-order/page.tsx
+++ frontend/Ciseco_Nextjs/CisecoNextjs/src/app/(accounts)/account-order/page.tsx
@@ -1,21 +1,19 @@
 import Prices from "@/components/Prices";
 import { PRODUCTS } from "@/data/data";
 import ButtonSecondary from "@/shared/Button/ButtonSecondary";
+import { color } from "framer-motion";
 import Image from "next/image";
 
 const AccountOrder = () => {
   const renderProductItem = (product: any, index: number) => {
-    const { image, name } = product;
+    const { image, name ,price, category} = product;
     return (
       <div key={index} className="flex py-4 sm:py-7 last:pb-0 first:pt-0">
         <div className="relative h-24 w-16 sm:w-20 flex-shrink-0 overflow-hidden rounded-xl bg-slate-100">
-          <Image
-            fill
-            sizes="100px"
-            src={image}
-            alt={name}
+          <h2
+          style={{backgroundImage: `url(${'https://images.pexels.com/photos/3631430/pexels-photo-3631430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'})`,fontSize: '120px'}}
             className="h-full w-full object-cover object-center"
-          />
+          >{name[0]}</h2>
         </div>
 
         <div className="ml-4 flex flex-1 flex-col">
@@ -24,19 +22,19 @@ const AccountOrder = () => {
               <div>
                 <h3 className="text-base font-medium line-clamp-1">{name}</h3>
                 <p className="mt-1 text-sm text-slate-500 dark:text-slate-400">
-                  <span>{"Natural"}</span>
+                  <span>{"Lend"}</span>
                   <span className="mx-2 border-l border-slate-200 dark:border-slate-700 h-4"></span>
-                  <span>{"XL"}</span>
+                  <span>{category}</span>
                 </p>
               </div>
-              <Prices className="mt-0.5 ml-2" />
+              <Prices className="mt-0.5 ml-2" price={price}/>
             </div>
           </div>
           <div className="flex flex-1 items-end justify-between text-sm">
             <p className="text-gray-500 dark:text-slate-400 flex items-center">
-              <span className="hidden sm:inline-block">Qty</span>
+              {/* <span className="hidden sm:inline-block">username</span>
               <span className="inline-block sm:hidden">x</span>
-              <span className="ml-2">1</span>
+              <span className="ml-2">Rahul Verma</span> */}
             </p>
 
             <div className="flex">
@@ -44,7 +42,7 @@ const AccountOrder = () => {
                 type="button"
                 className="font-medium text-indigo-600 dark:text-primary-500 "
               >
-                Leave review
+                View Transaction
               </button>
             </div>
           </div>
@@ -56,7 +54,7 @@ const AccountOrder = () => {
   const renderOrder = () => {
     return (
       <div className="border border-slate-200 dark:border-slate-700 rounded-lg overflow-hidden z-0">
-        <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center p-4 sm:p-8 bg-slate-50 dark:bg-slate-500/5">
+        {/* <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center p-4 sm:p-8 bg-slate-50 dark:bg-slate-500/5">
           <div>
             <p className="text-lg font-semibold">#WU3746HGG12</p>
             <p className="text-slate-500 dark:text-slate-400 text-sm mt-1.5 sm:mt-2">
@@ -73,9 +71,9 @@ const AccountOrder = () => {
               View Order
             </ButtonSecondary>
           </div>
-        </div>
+        </div> */}
         <div className="border-t border-slate-200 dark:border-slate-700 p-2 sm:p-8 divide-y divide-y-slate-200 dark:divide-slate-700">
-          {[PRODUCTS[0], PRODUCTS[1], PRODUCTS[2]].map(renderProductItem)}
+          {PRODUCTS.map(renderProductItem)}
         </div>
       </div>
     );
@@ -85,7 +83,6 @@ const AccountOrder = () => {
     <div className="space-y-10 sm:space-y-12">
       {/* HEADING */}
       <h2 className="text-2xl sm:text-3xl font-semibold">Order History</h2>
-      {renderOrder()}
       {renderOrder()}
     </div>
   );
